62e7e81e0b48008ffbdb6d9f354e3f7ea92e29a0
Ivanov Dmitriy <dmitriy.p86@ya.ru>
2022-01-22 19:46:05 +0600

    homepage_data_fetched

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,18 +7,6 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/'
 
-// const params = {
-//   referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//   timePeriod: '24h',
-//   tiers: '1',
-//   orderBy: 'marketCap',
-//   orderDirection: 'desc',
-//   limit: '50',
-//   offset: '0'
-// }
-
-// const createRequest = (url) => ({ url, params: params, headers: cryptoApiHeaders })
-
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
@@ -34,21 +22,3 @@ export const cryptoApi = createApi({
 export const {
   useGetCryptosQuery,
 } = cryptoApi;
-
-// var options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/coins',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     tiers: '1',
-//     orderBy: 'marketCap',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-//   },
-//   headers: {
-//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//     'x-rapidapi-key': 'be5274665amsh264fe6a875457d1p1cdf74jsncb240510b73d'
-//   }
-// };
