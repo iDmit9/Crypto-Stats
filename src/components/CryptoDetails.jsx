dc3745743be4525081bc1209f0afa818520e8e6b
Ivanov Dmitriy <dmitriy.p86@ya.ru>
2022-01-23 22:38:53 +0600

    chart_added_to_details

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -13,13 +13,10 @@ const { Option } = Select;
 
 const CryptoDetails = () => {  
   const { coinId } = useParams();
-  const [ timePeriod, setTimePeriod ] = useState('24h');
+  const [ timePeriod, setTimePeriod ] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const { data: coinHistory } = useGetCryptoHistoryQuery({coinId, timePeriod});
   const cryptoDetails = data?.data?.coin;
-
-  console.log('cryptoDetails', cryptoDetails);
-  console.log('coinHistory',coinHistory);
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title as ChartTitle,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
-const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ChartTitle,
+  Tooltip,
+  Legend
+);
 
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    if(!coinHistory?.data?.history[i].price) continue
+    coinPrice.push(coinHistory?.data?.history[i].price);    
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp * 1000).toLocaleDateString());
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {    
+    plugins: {    
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Price In USD',
+      },
+    },  
+    scales: {
+      y: {
+        beginAtZero: true,      
+      },
+      x:{
+        reverse: true,
+      }
+    },
+  };
+  
   return (
     <>
       <Row className="chart-header">
@@ -15,6 +75,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
+      <Line data={data} options={options} />
     </>
   )
 };
